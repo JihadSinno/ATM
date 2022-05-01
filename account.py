3a9ccdcfa36981561ac64b28ce45927ecf633871
JihadSinno <jihad99817@gmail.com>
2022-05-01 11:47:29 -0600

    basic structure

---
+++ account.py
@@ -0,0 +1,7 @@
+class Account:
+    def __init__(self,userId,accountName, accountId, balance):
+        self.userId= userId
+        self.accountName= accountName
+        self.accountId= accountId
+        self.balance= balance
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from account import *
+a1=Account(12, "Savings Account", "B213","5000")
+print("User ID:"+ str(a1.userId))
+print("Account:"+ str(a1.accountName))
+print("Account ID:" + str(a1.accountId))
+print("Balance:" + str(a1.balance) + " USD")
