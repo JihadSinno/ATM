b0a692c5ef01b3fae39552f8774ac84c4cb053c5
JihadSinno <jihad99817@gmail.com>
2022-05-15 11:38:50 -0600

    Handled insufficient funds and negative funds in account class for addFunds, transferAmount, and withdrawFunds.

---
+++ account_exceptions.py
@@ -0,0 +1,13 @@
+
+
+class InsufficientFundsException(Exception):
+    pass
+
+class NegativeTransferException(Exception):
+    pass
+    
+class NegativeAddFundsException(Exception):
+    pass
+
+class NegativeWithdrawFundsException(Exception):
+    pass
